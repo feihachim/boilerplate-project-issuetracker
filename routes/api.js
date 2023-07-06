0dee26912db603017afe543a71f701ead1d7fc67
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-07-06 21:37:09 +0200

    created issue Object?

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,48 @@
-'use strict';
+"use strict";
+
+const { v4: uuidv4 } = require("uuid");
+
+const issueFactory = (
+  issue_title,
+  issue_text,
+  created_by,
+  assigned_to,
+  status_text
+) => {
+  const _id = uuidv4();
+  const created_on = new Date();
+  const updated_on = new Date();
+  const open = true;
+  return {
+    _id,
+    issue_title,
+    issue_text,
+    created_on,
+    updated_on,
+    created_by,
+    assigned_to,
+    open,
+    status_text,
+  };
+};
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
