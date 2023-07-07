697f80c789743a19fa772407b58e937606cb812c
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-07-08 00:03:43 +0200

    POST request DONE

--- routes/api.js
+++ routes/api.js
@@ -26,6 +26,8 @@ const issueFactory = (
   };
 };
 
+let projectsArray = [];
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
@@ -36,6 +38,36 @@ module.exports = function (app) {
 
     .post(function (req, res) {
       let project = req.params.project;
+      let issueList = projectsArray.filter((value) => value.name === project);
+      let newProject;
+      if (issueList.length === 0) {
+        newProject = {
+          name: project,
+          value: [],
+        };
+        projectsArray.push(newProject);
+      } else {
+        newProject = issueList[0];
+      }
+
+      const issue_title = req.body.issue_title ?? undefined;
+      const issue_text = req.body.issue_text ?? undefined;
+      const created_by = req.body.created_by ?? undefined;
+      const assigned_to = req.body.assigned_to ?? "";
+      const status_text = req.body.status_text ?? "";
+      if (!issue_title || !issue_text || !created_by) {
+        res.send({ error: "required field(s) missing" });
+        return;
+      }
+      const newIssue = issueFactory(
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text
+      );
+      newProject.value.push(newIssue);
+      res.status(200).send(newIssue);
     })
 
     .put(function (req, res) {
