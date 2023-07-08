aa5820e8cbfe667cefbca0b29492b195a19e0f13
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-07-08 13:06:59 +0200

    written tests for POST requests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,68 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  // #1
+  test("create an issue with every field", function (done) {
+    chai
+      .request(server)
+      .post("/api/issues/apitest")
+      .send({
+        issue_title: "test issue",
+        issue_text: "test text",
+        created_by: "Joe",
+        assigned_to: "Hachim",
+        status_text: "Pending",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body.issue_title, "test issue");
+        assert.equal(res.body.issue_text, "test text");
+        assert.equal(res.body.created_by, "Joe");
+        assert.equal(res.body.assigned_to, "Hachim");
+        assert.equal(res.body.status_text, "Pending");
+        done();
+      });
+  });
+  // #2
+  test("create an issue with only requred fields", function (done) {
+    chai
+      .request(server)
+      .post("/api/issues/apitest")
+      .send({
+        issue_title: "test 2 issue",
+        issue_text: "test text 2",
+        created_by: "John",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body.issue_title, "test 2 issue");
+        assert.equal(res.body.issue_text, "test text 2");
+        assert.equal(res.body.created_by, "John");
+        assert.equal(res.body.assigned_to, "");
+        assert.equal(res.body.status_text, "");
+        done();
+      });
+  });
+  // #3
+  test("create an issue with missing required fields", function (done) {
+    chai
+      .request(server)
+      .post("/api/issues/apitest")
+      .send({
+        issue_text: "test text 3",
+        created_by: "John",
+      })
+      .end(function (err, res) {
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body.error, "required field(s) missing");
+        done();
+      });
+  });
 });
